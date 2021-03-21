71388284cd0b6e946fa504232eb9597616e18eaa
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2021-03-21 14:13:19 +0300

    Renamed account urls

--- web_chat/urls.py
+++ web_chat/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('apps.account.urls')),
+    path('account/', include('apps.account.urls')),
     path('', include('apps.chat.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
