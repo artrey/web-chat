296aa997f68a9abb937f40bf686ada0e806ab64f
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2021-03-28 16:20:44 +0300

    Fixed register form

--- apps/account/forms.py
+++ apps/account/forms.py
@@ -1,13 +1,12 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django_registration.forms import RegistrationForm
 
 from apps.account.models import User
 
 
-class RegisterForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
+class RegisterForm(RegistrationForm):
+    class Meta(RegistrationForm.Meta):
         model = User
-        fields = ('username', 'email')
 
 
 class ProfileForm(forms.ModelForm):
